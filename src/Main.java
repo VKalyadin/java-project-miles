5a574b3cba0171a507dfb1cf46e0d0eaad0b98ff
VKalyadin <vkalyadin87@gmail.com>
2023-01-10 06:48:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 2000;
+        int amount = 20;
+
+
+        int miles = price / amount;
+
+        System.out.println("Начисленные мили: " + miles);
+        
+    }
+}
